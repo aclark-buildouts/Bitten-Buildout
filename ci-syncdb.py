1f0a47db8fd5609ae370b01d00b08cdbd0c84f6a
Alex Clark <aclark@aclark.net>
2010-09-12 22:03:12 -0400

    Fix CI check

--- ci-syncdb.py
+++ ci-syncdb.py
@@ -5,12 +5,17 @@ SUCCESS=0
 FAILURE=-1
 
 south_failure = 'these migrations are in the database but not on disk'
-pattern = re.compile(south_failure)
+generic_traceback = 'Traceback (most recent call last)'
+
+check_for = south_failure, generic_traceback
+
 results = commands.getoutput('bin/django syncdb --migrate')
 
-if not pattern.match(results):
-    print results
-    exit(SUCCESS)
-else:
-    print results
-    exit(FAILURE)
+for error in check_for:
+    pattern = re.compile(error)
+    if not pattern.match(results):
+        print results
+        exit(SUCCESS)
+    else:
+        print results
+        exit(FAILURE)
