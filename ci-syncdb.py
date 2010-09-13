5fa0e22613dc4f23743897fca84c06c49ef5352c
Alex Clark <aclark@aclark.net>
2010-09-12 21:40:11 -0400

    Call exit correctly

--- ci-syncdb.py
+++ ci-syncdb.py
@@ -1,11 +1,14 @@
 import commands
 import re
 
+SUCCESS=0
+FAILURE=-1
+
 south_failure = 'these migrations are in the database but not on disk'
 pattern = re.compile(south_failure)
 results = commands.getoutput('bin/django syncdb --migrate')
 
 if not results.match(pattern):
-    exit 0
+    exit(SUCCESS)
 else:
-    exit -1
+    exit(FAILURE)
