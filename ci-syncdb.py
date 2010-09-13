1311f7a77a0cbbb0d55feb9aacaddb1f4f79bc56
Alex Clark <aclark@aclark.net>
2010-09-12 21:39:13 -0400

    exit, not return

--- ci-syncdb.py
+++ ci-syncdb.py
@@ -6,6 +6,6 @@ pattern = re.compile(south_failure)
 results = commands.getoutput('bin/django syncdb --migrate')
 
 if not results.match(pattern):
-    return True
+    exit 0
 else:
-    return False
+    exit -1
